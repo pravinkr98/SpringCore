2243fadf14dda7922533862d56ac17dbb4f449c5
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-05-13 22:47:41 +0530

    DependencyLookup-AwareInjection

---
+++ IOCProj41-MobilePhone-DependencyLookup-AwareInjection-AC/src/com/ps/beans/MobilePhone.java
@@ -0,0 +1,56 @@
+package com.ps.beans;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MobilePhone implements ApplicationContextAware {
+	private String beanId;
+	ApplicationContext ctx;
+	
+	public MobilePhone() {
+			System.out.println("MobilePhone.:: 0-param constructor");
+	}
+			
+	public void setBeanId(String beanId) {
+		System.out.println("MobilePhone.setBeanId()");
+		this.beanId = beanId;
+	}
+	
+	public void entertainment() {
+		System.out.println("Gives entertainment using Apps like prime video,netflix,browser and...");
+	}
+	
+	public void gaming() {
+		System.out.println("Gives Gaming feeling using PubG, ludo king, free fire and etc...apps");
+	}
+	
+	@Override
+	public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+		this.ctx=ctx;
+		
+	}
+	
+	public void batteryCharging() {
+		MobilePhoneBatteryCharger charger=null;
+		
+		System.out.println("MobilePhone.batteryCharging()");
+		System.out.println("2nd IOC container");
+	
+		//get Dependent class object
+		charger=ctx.getBean(beanId,MobilePhoneBatteryCharger.class);
+		charger.charging();
+	}
+
+	public void calls() {
+		System.out.println("Handling both incoming and outgoing calls");
+	}
+
+	public void shopping() {
+		System.out.println("Online shopping using amazon,flipkart and etc...apps");
+	}
+
+	
+
+}
