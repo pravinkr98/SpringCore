2b84275d576440a9ded4fc62e0634bf99dfe721a
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-06-03 14:08:19 +0530

    SpringBootOlderVersion

---
+++ IOCBootProj67-StudentLayeredApp-SpringOldVer/src/main/java/com/ps/IocBootProj67StudentLayeredAppSpringOldVerApplication.java
@@ -0,0 +1,63 @@
+package com.ps;
+
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.ps.controller.MainController;
+import com.ps.vo.StudentVO;
+
+@SpringBootApplication
+public class IocBootProj67StudentLayeredAppSpringOldVerApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=null;
+		Scanner sc=null;
+		String sname,sadd,m1,m2,m3;
+		MainController controller=null;
+		String result=null;
+		StudentVO vo=null;
+		
+		//create IOC container
+		ctx=SpringApplication.run(IocBootProj67StudentLayeredAppSpringOldVerApplication.class, args);
+		//read input(5)sname,sadd,m1,m2,m3 from enduser and keep them in StudentVO class obj as inputs (String)
+		sc=new Scanner(System.in);
+		System.out.println("Input student sname :: ");
+		sname=sc.next();
+		System.out.println("Input student sadd :: ");
+		sadd=sc.next();
+		System.out.println("Input student marks 1 :: ");
+		m1=sc.next();
+		System.out.println("Input student marks 2 :: ");
+		m2=sc.next();
+		System.out.println("Input student marks 3 :: ");
+		m3=sc.next();
+		
+		vo=new StudentVO();
+		vo.setSname(sname);
+		vo.setSadd(sadd);
+		vo.setM1(m1);
+		vo.setM2(m2);
+		vo.setM3(m3);
+		
+		//get Controller class obj
+		controller=ctx.getBean("stdController",MainController.class);
+		
+		try {
+			//invoke methods
+			result=controller.handle(vo);
+			System.out.println(result);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		//close container
+		((ConfigurableApplicationContext) ctx).close();
+
+	}//main
+
+}
---
+++ IOCBootProj67-StudentLayeredApp-SpringOldVer/src/test/java/com/ps/IocBootProj67StudentLayeredAppSpringOldVerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocBootProj67StudentLayeredAppSpringOldVerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
