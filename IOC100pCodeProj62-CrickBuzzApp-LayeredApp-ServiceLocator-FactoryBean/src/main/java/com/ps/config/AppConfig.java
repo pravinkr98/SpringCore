6c67651e3579e362b3710ff8691890c712a4f978
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-06-03 14:04:29 +0530

    100pServiceLocator

---
+++ IOC100pCodeProj62-CrickBuzzApp-LayeredApp-ServiceLocator-FactoryBean/src/main/java/com/ps/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.ps.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+//@ComponentScan(basePackages = {"com.ps.service","com.ps.locator"})
+@ComponentScan(basePackages = "com.ps")
+@ImportResource(locations = "com/ps/cfgs/applicationContext.xml")
+public class AppConfig {
+
+}
---
+++ IOC100pCodeProj62-CrickBuzzApp-LayeredApp-ServiceLocator-FactoryBean/src/main/java/com/ps/external/ExternalIccScoreComp.java
@@ -0,0 +1,5 @@
+package com.ps.external;
+
+public interface ExternalIccScoreComp {
+	public String getScore(int mid);
+}
